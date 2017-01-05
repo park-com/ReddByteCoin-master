c05699cbde0b32648e768d30264e17c2000a389c
Denlon210 <denlon210@gmail.com>
2017-01-05 12:54:40 +0400

    Update main.h

--- src/main.h
+++ src/main.h
@@ -37,7 +37,7 @@ static const unsigned int MAX_ORPHAN_TRANSACTIONS = MAX_BLOCK_SIZE/100;
 static const unsigned int MAX_INV_SZ = 50000;
 static const int64_t MIN_TX_FEE = 100000;
 static const int64_t MIN_RELAY_TX_FEE = MIN_TX_FEE;
-static const int64_t MAX_MONEY = 69000000 * COIN;
+static const int64_t MAX_MONEY = 690000000 * COIN;
 
 static const int64_t COIN_YEAR_REWARD = 40 * CENT;
 static const int64_t MAX_MINT_PROOF_OF_STAKE = 1 * COIN;
