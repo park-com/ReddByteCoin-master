c61622c243c5f56d5e75690ed8d177a225ce0e7b
Denlon210 <denlon210@gmail.com>
2017-01-23 18:50:47 +0300

    Update autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -3,5 +3,3 @@
 sudo apt-get install miniupnpc libdb++-dev build-essential autoconf libtool libboost-all-dev libcurl4-openssl-dev libdb5.1-dev libdb5.1++dev libboost-chrono-dev libboost-filesystem-dev libboost-program-options-dev libboost-system-dev libboost-test-dev libboost-thread-dev libqt4-dev libprotobuf-dev protobuf-compiler libqrencode-dev -y
 cd src
 make -f makefile.unix
-cd ../
-cp scr/ReddByted ReddByted
