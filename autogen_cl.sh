e4ae11eef12e2ed5f3103fa8818dfac047158dbe
Denlon210 <denlon210@gmail.com>
2017-09-16 15:34:50 +0300

    New wallet version

--- autogen_cl.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# Copyright (c) 2017 The ReddByteCoin Core developers
-sudo apt-get install miniupnpc libdb++-dev build-essential autoconf libtool libboost-all-dev libcurl4-openssl-dev libdb5.1-dev libdb5.1++dev libboost-chrono-dev libboost-filesystem-dev libboost-program-options-dev libboost-system-dev libboost-test-dev libboost-thread-dev libqt4-dev libprotobuf-dev protobuf-compiler libqrencode-dev -y
-cd src
-make -f makefile.unix
---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# Copyright (c) 2017 The ReddByteCoin developers.
+sudo apt-get install libssl-dev openssl git miniupnpc libdb-dev libdb++-dev build-essential libgmp-dev autoconf libtool libboost-all-dev libcurl4-openssl-dev git libboost-chrono-dev libboost-filesystem-dev libboost-program-options-dev libboost-system-dev libboost-test-dev libboost-thread-dev libprotobuf-dev protobuf-compiler libqrencode-dev -y
+cd src
+make -f makefile.unix
