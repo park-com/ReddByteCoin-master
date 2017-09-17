706036f0590913a7f37577dba1f5eb43cdc0673e
Denlon210 <denlon210@gmail.com>
2017-09-17 21:01:42 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 # Copyright (c) 2017 The ReddByteCoin developers.
-sudo apt-get install libssl-dev openssl git miniupnpc libdb-dev libdb++-dev build-essential libgmp-dev autoconf libtool libboost-all-dev libcurl4-openssl-dev git libboost-chrono-dev libboost-filesystem-dev libboost-program-options-dev libboost-system-dev libboost-test-dev libboost-thread-dev libprotobuf-dev protobuf-compiler libqrencode-dev -y
+sudo apt-get install g++ git gcc -y
+sudo apt-get install libssl-dev openssl miniupnpc libdb-dev libdb++-dev build-essential libgmp-dev autoconf libtool libboost-all-dev libcurl4-openssl-dev git libboost-chrono-dev libboost-filesystem-dev libboost-program-options-dev libboost-system-dev libboost-test-dev libboost-thread-dev libprotobuf-dev protobuf-compiler libqrencode-dev -y
+rm ReddByted
 cd src
-make -f makefile.unix
+make -f makefile.unix -j
+cp ReddByted ../
+rm ReddByted
+cd ../
