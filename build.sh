99f65d1dff4f7f5bfc3402a0199d4447857e1e24
Denlon210 <denlon210@gmail.com>
2017-09-24 21:01:56 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ sudo apt-get install g++ git gcc -y
 sudo apt-get install libssl-dev openssl miniupnpc libdb-dev libdb++-dev build-essential libgmp-dev autoconf libtool libboost-all-dev libcurl4-openssl-dev git libboost-chrono-dev libboost-filesystem-dev libboost-program-options-dev libboost-system-dev libboost-test-dev libboost-thread-dev libprotobuf-dev protobuf-compiler libqrencode-dev -y
 rm ReddByted
 cd src
-make -f makefile.unix -j
+make -f makefile.unix
 cp ReddByted ../
 rm ReddByted
 cd ../
